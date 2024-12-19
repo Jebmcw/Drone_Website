8071ee2fac67dc59270d9cc9ab8058558263a7a7
Blake Woods <woods.blake03@gmail.com>
2024-12-19 10:14:32 -0600

    fix the contact bug and made the home page and about page better

--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './HeroSection.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./HeroSection.css";
 
 const HeroSection = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
   return (
     <section className="hero-section">
-      <div className="hero-content">
-        <h1 className="hero-title">CW Aerial Solutions
-        </h1>
-        <p className="hero-subtitle">
+      <div className="hero-overlay"></div>
+      <div className="hero-content" data-aos="fade-down">
+        <h1 className="hero-title">CW Aerial Solutions</h1>
+        <p className="hero-subtitle" data-aos="fade-up">
           Beautiful, Elegantly Coded, One-Page Theme for Your Business
         </p>
-        {/* Link the button to the "/explained" route */}
-        <Link to="explained">
-          <button className="hero-button">Contact Us</button>
+        <Link to="/explained">
+          <button className="hero-button" data-aos="zoom-in">Contact Us</button>
         </Link>
       </div>
     </section>
